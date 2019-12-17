6c5f7e3dec797bc4976465e27c6dc6d3c1ad4e41
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2019-12-17 16:57:06 +0100

    Changed.

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+<footer class="site-footer">
+      <div class="text-center">
+        <div class="credits">
+          Created as a part of Hackathon
+        </div>
+        <a href="advanced_table.html#" class="go-top">
+          <i class="fa fa-angle-up"></i>
+          </a>
+      </div>
+      <?php include './inc/db-close.php';?>
+</footer>
